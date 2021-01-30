bca9ffe6e555513e5ee4f3961500b6caad31011a
Xiangyu Peng <pengxy@berkeley.edu>
2021-01-30 13:21:48 +0800

    m

--- mmdet3d/apis/__init__.py
+++ mmdet3d/apis/__init__.py
@@ -1,10 +1,12 @@
 from .inference import inference_detector, init_detector, show_result_meshlab
 from .test import single_gpu_test, mmda_single_gpu_test
-from .train import parse_losses, set_random_seed, train_detector, rep_train_detector
+from .utils import parse_losses
+from .train import set_random_seed, train_detector, rep_train_detector
 
 
 __all__ = [
     'inference_detector', 'init_detector', 'single_gpu_test',
     'show_result_meshlab', 'mmda_single_gpu_test',
-    'parse_losses', 'set_random_seed', 'train_detector', 'rep_train_detector',
+    'parse_losses',
+    'set_random_seed', 'train_detector', 'rep_train_detector',
 ]
--- mmdet3d/apis/train.py
+++ mmdet3d/apis/train.py
@@ -13,34 +13,6 @@ from mmdet.utils import get_root_logger
 from mmdet3d.models import build_discriminator
 from mmdet3d.runners import DiscRunner, RepRunner
 
-from collections import OrderedDict
-import torch.distributed as dist
-
-
-def parse_losses(losses):
-    log_vars = OrderedDict()
-    for loss_name, loss_value in losses.items():
-        if isinstance(loss_value, torch.Tensor):
-            log_vars[loss_name] = loss_value.mean()
-        elif isinstance(loss_value, list):
-            log_vars[loss_name] = sum(_loss.mean() for _loss in loss_value)
-        else:
-            raise TypeError(
-                f'{loss_name} is not a tensor or list of tensors')
-
-    loss = sum(_value for _key, _value in log_vars.items()
-               if 'loss' in _key)
-
-    log_vars['loss'] = loss
-    for loss_name, loss_value in log_vars.items():
-        # reduce loss when distributed training
-        if dist.is_available() and dist.is_initialized():
-            loss_value = loss_value.data.clone()
-            dist.all_reduce(loss_value.div_(dist.get_world_size()))
-        log_vars[loss_name] = loss_value.item()
-
-    return loss, log_vars
-
 
 def set_random_seed(seed, deterministic=False):
     random.seed(seed)
