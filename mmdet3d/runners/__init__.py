860ea23e559a9f6f14aee2db4708102162c5adc6
Xiangyu Peng <pengxy@berkeley.edu>
2021-02-04 14:53:27 +0800

    m

--- mmdet3d/runners/__init__.py
+++ mmdet3d/runners/__init__.py
@@ -4,4 +4,5 @@ from .disc_runner_03 import DiscRunner03
 from .disc_runner_04 import DiscRunner04
 from .disc_runner_05 import DiscRunner05
 from .disc_runner_06 import DiscRunner06
+from .disc_runner_07 import DiscRunner07
 from .reproduce_runner import RepRunner
