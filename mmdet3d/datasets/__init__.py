8219116dbf945af84365769da420e475c2754f4e
Xiangyu Peng <pengxy@berkeley.edu>
2021-03-16 13:31:50 +0800

    m

--- mmdet3d/datasets/__init__.py
+++ mmdet3d/datasets/__init__.py
@@ -1,8 +1,6 @@
 from mmdet.datasets.builder import build_dataloader
 from .builder import DATASETS, build_dataset
 from .custom_3d import Custom3DDataset
-from .kitti_dataset import KittiDataset
-from .lyft_dataset import LyftDataset
 from .nuscenes_dataset import NuScenesDataset
 from .nusc_multi_modal_dataset import NuscMultiModalDataset
 from .mmda_merge_cat_dataset import MMDAMergeCatDataset
@@ -15,21 +13,18 @@ from .pipelines import (BackgroundPointsFilter, GlobalRotScaleTrans,
                         RandomFlip3D, VoxelBasedPointSampler,
                         LoadSegDetPointsFromFile, LoadFrontImage, SegDetPointsRangeFilter,
                         SegDetFormatBundle)
-from .scannet_dataset import ScanNetDataset
-from .sunrgbd_dataset import SUNRGBDDataset
-from .waymo_dataset import WaymoDataset
 
-__all__ = [
-    'KittiDataset', 'GroupSampler', 'DistributedGroupSampler',
-    'build_dataloader', 'RepeatFactorDataset', 'DATASETS', 'build_dataset',
-    'CocoDataset', 'NuScenesDataset', 'LyftDataset', 'ObjectSample',
-    'RandomFlip3D', 'ObjectNoise', 'GlobalRotScaleTrans', 'PointShuffle',
-    'ObjectRangeFilter', 'PointsRangeFilter', 'Collect3D',
-    'LoadPointsFromFile', 'NormalizePointsColor', 'IndoorPointSample',
-    'LoadAnnotations3D', 'SUNRGBDDataset', 'ScanNetDataset', 'Custom3DDataset',
-    'LoadPointsFromMultiSweeps', 'WaymoDataset', 'BackgroundPointsFilter',
-    'VoxelBasedPointSampler',
-    # added
-    'LoadSegDetPointsFromFile', 'LoadFrontImage', 'SegDetPointsRangeFilter',
-    'SegDetFormatBundle', 'NuscMultiModalDataset', 'MMDAMergeCatDataset'
-]
+# __all__ = [
+#     'KittiDataset', 'GroupSampler', 'DistributedGroupSampler',
+#     'build_dataloader', 'RepeatFactorDataset', 'DATASETS', 'build_dataset',
+#     'CocoDataset', 'NuScenesDataset', 'LyftDataset', 'ObjectSample',
+#     'RandomFlip3D', 'ObjectNoise', 'GlobalRotScaleTrans', 'PointShuffle',
+#     'ObjectRangeFilter', 'PointsRangeFilter', 'Collect3D',
+#     'LoadPointsFromFile', 'NormalizePointsColor', 'IndoorPointSample',
+#     'LoadAnnotations3D', 'SUNRGBDDataset', 'ScanNetDataset', 'Custom3DDataset',
+#     'LoadPointsFromMultiSweeps', 'WaymoDataset', 'BackgroundPointsFilter',
+#     'VoxelBasedPointSampler',
+#     # added
+#     'LoadSegDetPointsFromFile', 'LoadFrontImage', 'SegDetPointsRangeFilter',
+#     'SegDetFormatBundle', 'NuscMultiModalDataset', 'MMDAMergeCatDataset'
+# ]
