ee14866f639f8b759e5aad72f360b5ccf49d6783
Xiangyu Peng <pengxy@berkeley.edu>
2021-04-06 11:44:27 +0800

    m

--- scripts/loss_curve.py
+++ scripts/loss_curve.py
@@ -1,37 +1,70 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from collections import defaultdict
+import os
 
-plt_colors = ['c', 'r', 'g', 'b', 'y', 'k', 'm', '#2A0134', '#FF00FF', '#800000']
-plt_markers = ['*', '.', 'o', '^', 'v', '<', '>', '1', '2', '3', '4', 's', 'p', ',']
+plt_colors = iter(['c', 'y', 'g', 'm', 'r', 'k', 'b', '#2A0134', '#FF00FF', '#800000'])
+plt_markers = iter(['*', '.', 'o', '^', 'v', '<', '>', '1', '2', '3', '4', 's', 'p', ','])
 font = {
     'size': 18
 }
 
 src_domain, tgt_domain = 'usa', 'sng'
-sub_dir = 'only_ctr_usa_v1'
-log_file = f'../checkpoints/fusion_consis/xmuda/{sub_dir}/log.log'  # to add: tgt_val, src_val
+sub_dir = 'src_ctr_usa_v3'
+log_train = f'../checkpoints/fusion_consis/xmuda/{sub_dir}/log.log'  # to add: tgt_val, src_val
+log_src_test = f'../checkpoints/fusion_consis/xmuda/{sub_dir}/source_test.log'
+log_tgt_test = f'../checkpoints/fusion_consis/xmuda/{sub_dir}/target_test.log'
 
 
-accf = open(log_file, 'r')
-lines = accf.readlines()
-accf.close()
-losses = defaultdict(list)
-for line in lines:
-    if 'Epoch' in line:
-        splits = line.split(', ')
-        for token in splits:
-            for loss_type in ['contrast_loss', 'seg_loss', 'tgt_contrast_loss']:
-                if token.startswith(loss_type):
-                    losses[loss_type].append(float(token.split(': ')[1]))
-if 'contrast_loss' in losses.keys():
-    losses['src_contrast_loss'] = losses['contrast_loss']
-    del losses['contrast_loss']
-for k, v in losses.items():
-    x_len = len(v)
-    break
-# x_len = len(losses['seg_loss'])
-x_range = np.arange(x_len) + 1
+def plot_train(log_file, epochs=24):
+    if not os.path.exists(log_file):
+        print(f'\'{log_file}\' not exists!')
+    accf = open(log_file, 'r')
+    lines = accf.readlines()
+    accf.close()
+    losses = defaultdict(list)
+    for line in lines:
+        if 'Epoch' in line:
+            splits = line.split(', ')
+            for token in splits:
+                for loss_type in ['contrast_loss', 'seg_loss', 'tgt_contrast_loss']:
+                    if token.startswith(loss_type):
+                        losses[loss_type].append(float(token.split(': ')[1]))
+    if 'contrast_loss' in losses.keys():
+        losses['train_src_ctrloss'] = losses['contrast_loss']
+        del losses['contrast_loss']
+    if 'seg_loss' in losses.keys():
+        losses['train_segloss'] = losses['seg_loss']
+        del losses['seg_loss']
+    for k, v in losses.items():
+        x_len = len(v)
+        break
+    xs = np.arange(x_len) + 1
+    for i, (key, val) in enumerate(losses.items()):
+        ys = np.array(val)
+        plt.plot(xs, ys, label=key, color=next(plt_colors), linewidth=0.9)
+    plt.xticks(range(0, x_len, x_len // epochs), labels=range(1, epochs+1))
+    return x_len
+
+
+def plot_test(log_file, iters=1872, curve_name='src'):
+    if not os.path.exists(log_file):
+        print(f'\'{log_file}\' not exists!')
+    accf = open(log_file, 'r')
+    lines = accf.readlines()
+    accf.close()
+    losses = defaultdict(list)
+    for line in lines:
+        if 'seg_loss' in line:
+            losses['test_' + curve_name + '_segloss'].append(float(line.split(': ')[1]))
+    for k, v in losses.items():
+        x_len = len(v)
+        break
+    xs = np.arange(0, iters, iters // x_len)
+    for i, (key, val) in enumerate(losses.items()):
+        ys = np.array(val)
+        plt.plot(xs, ys, label=key, color=next(plt_colors), linewidth=0.9, linestyle='--')
+    # plt.xticks(range(0, x_len, x_len // epochs), labels=range(1, epochs+1))
 
 
 if __name__ == '__main__':
@@ -39,10 +72,10 @@ if __name__ == '__main__':
     # plt.xlabel('log_interval(every 25 batches)', font)
     plt.xlabel('epoch', font)
     plt.ylabel('loss', font)
-    for i, (key, val) in enumerate(losses.items()):
-        val = np.array(val)
-        plt.plot(x_range, val, label=key, color=plt_colors[i], linewidth=1.)
-    plt.xticks(range(0, x_len, x_len // 24), labels=range(0, 25))
+    iters = plot_train(log_train)
+    plot_test(log_src_test, iters, curve_name='src')
+    plot_test(log_tgt_test, iters, curve_name='tgt')
+
     plt.legend(loc='best', prop=font)
     plt.ylim(bottom=0)
     plt.show()
