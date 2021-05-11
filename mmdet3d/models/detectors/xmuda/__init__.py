f307df77d44b921825f262f11db7bbf6309277c6
Xiangyu Peng <pengxy@berkeley.edu>
2021-05-11 13:42:59 +0800

    m

--- mmdet3d/models/detectors/xmuda/__init__.py
+++ mmdet3d/models/detectors/xmuda/__init__.py
@@ -4,3 +4,4 @@ from .seg_fusion_v3 import SegFusionV3
 from .single_2d_3d import Single2D3D
 from .single_2d_3d_v2 import Single2D3DV2
 from .seg_fusion_v4 import SegFusionV4
+from .seg_fusion_contra import SegFusionContra
